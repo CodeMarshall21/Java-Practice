e079eb32c6e887f94cd756f94a406a071d833504
Keshav <k21keshav05@gmail.com>
2025-01-12 01:07:32 +0530

    mad-libs

--- BroCode/src/Inputs.java
+++ BroCode/src/Inputs.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class inputs {
+public class Inputs {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
--- BroCode/src/Madlibs.java
+++ BroCode/src/Madlibs.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class madlibs {
+public class Madlibs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
