c56ba896db637892d7ac19fcec989019b341dbad
Keshav <k21keshav05@gmail.com>
2025-01-23 11:19:28 +0530

    Multiple-ifStatements

--- BroCode/src/RandomNumber.java
+++ BroCode/src/RandomNumber.java
@@ -9,18 +9,24 @@ public class RandomNumber {
         int Small_random_number = random.nextInt(1,10); //Range -> 1 to 9
 
         //Decimal Randoms
-        double decimal_random = random.nextDouble();
-        double float_random = random.nextFloat();
+        double decimal_random = random.nextDouble(); //ranges between 0.0 (inclusive) and 1.0 (exclusive).
+
+        int sml = random.nextInt(100, 201);
+        double number = decimal_random + (double)Large_random_number;
+        double number2 = decimal_random + (double)sml;
 
         //Boolean Randoms
         boolean isHeads = random.nextBoolean();
 
 
 
-        System.out.println(Large_random_number);
-        System.out.println(Small_random_number);
-        System.out.println(decimal_random);
-        System.out.println(float_random);
+
+        System.out.println("Large_random_number: " + Large_random_number);
+        System.out.println("Small_random_number: " + Small_random_number);
+        System.out.println("sml: " + sml);
+        System.out.println("decimal_random: "+decimal_random);
+        System.out.println("number " +number);
+        System.out.println("number2: "+number2);
         if (isHeads){
             System.out.println("Heads !");
         }else{
