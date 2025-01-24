e2e2706148b4cf933565d7bd4e13bcb66814807d
Keshav <k21keshav05@gmail.com>
2025-01-24 16:02:25 +0530

    compund-interest

---
+++ BroCode/src/CompoundInterest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // a = p(1+(r/n))^nt
+
+        double amount;
+        double principal;
+        double RateOfInterest;
+        int NumberOfTimes;
+        int years;
+
+        System.out.print("Principal : ");
+        principal = input.nextDouble();
+
+        System.out.print("Rate Of Interest in % : ");
+        RateOfInterest = input.nextDouble() / 100;
+
+        System.out.print("Number Of Years : ");
+        NumberOfTimes = input.nextInt();
+
+        System.out.print("Times Compounded : ");
+        years = input.nextInt();
+
+        amount = principal* Math.pow((1+(RateOfInterest/NumberOfTimes)), NumberOfTimes*years);
+
+        System.out.printf("Amount after %d years is ₹%.2f", years,amount);
+
+        input.close();
+
+    }
+}
