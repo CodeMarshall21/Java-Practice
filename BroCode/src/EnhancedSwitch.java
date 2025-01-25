37bf78376cc077132dad7c8e00d49e6297cae62b
Keshav <k21keshav05@gmail.com>
2025-01-25 16:03:03 +0530

    enhanced-switches

---
+++ BroCode/src/EnhancedSwitch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EnhancedSwitch {
+    public static void main(String[] args) {
+        // Enhanced switch = A replacement to many else if statements
+        //                                   (Java14 feature)
+        
+        Scanner input = new Scanner(System.in);
+
+        String day;
+
+        System.out.print("Enter a day : ");
+        day = input.nextLine();
+
+        switch (day){
+            case "monday" , "tuesday" , "wednesday" , "thursday" , "friday" -> System.out.println("Weekday");
+            case "saturday" , "sunday" -> System.out.println("Weekend");
+            default -> System.out.printf("%s is not a day", day);
+        }
+
+    }
+}
