c5a428b36f2b629c9f01c70747374b9f8edab197
Keshav <k21keshav05@gmail.com>
2025-01-27 15:35:22 +0530

    Logical-Operators

---
+++ BroCode/src/LogicalOperators.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        String username;
+        System.out.print("Username : ");
+        username = input.nextLine();
+
+        if (username.length() < 4 || username.length() > 12){
+            System.out.println("out of range");
+        }
+        else if (username.contains(" ") || username.contains("_")){
+            System.out.println("no special character");
+        }
+        else{
+            System.out.printf("hello %s", username);
+        }
+
+        input.close();
+    }
+}
