825a9809c476197e02a05efb2996bcd340a01d8f
Keshav <k21keshav05@gmail.com>
2025-02-09 00:01:51 +0530

    reverse array

--- Kunal/Introduction to Arrays and ArrayList in Java/src/ReverseArray.java
+++ Kunal/Introduction to Arrays and ArrayList in Java/src/ReverseArray.java
@@ -4,20 +4,26 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5};
 
-        System.out.println(Arrays.toString(reverseArray(array)));
+        reverseArray(array);
+
+        System.out.println(Arrays.toString(array));
     }
-    static int[] reverseArray (int [] array){
+    static void reverseArray (int [] array){
         int i = 0;
         int j = array.length - 1;
-        int temp;
         while (i < j){
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-
+            swap(array, i,j);
             i++;
             j--;
         }
-        return array;
+//        return array;
+    }
+
+    static void swap (int [] array, int i, int j){
+        int temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
     }
 }
