a619952273cdb5755809bef3f7be479bdecc04ff
Keshav <k21keshav05@gmail.com>
2025-02-10 11:07:58 +0530

    Stack

---
+++ BroCode/Data Structures/src/com/stacks/StackCreation.java
@@ -0,0 +1,16 @@
+package com.stacks;
+
+import java.util.Stack;
+
+public class StackCreation {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        System.out.println(stack.empty());
+        stack.push("Item 1");
+        stack.push("Item 2");
+        stack.push("Item 3");
+        stack.push("Item 4");
+        System.out.println(stack.empty());
+
+    }
+}
