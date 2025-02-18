8489fa5acb6e45e24d3975486e56ab8da67c06f0
Keshav <k21keshav05@gmail.com>
2025-02-19 01:17:49 +0530

    Classes

---
+++ CodingWithJohn/WorkingWithClasses/src/com/classes/Cat.java
@@ -0,0 +1,13 @@
+package com.classes;
+
+public class Cat {
+    String name;
+    int age;
+
+    public void meow(){
+        System.out.println("Meow !");
+    }
+    public static void dingdong(){
+        System.out.println("Ding Dong");
+    }
+}
---
+++ CodingWithJohn/WorkingWithClasses/src/com/classes/Main.java
@@ -0,0 +1,15 @@
+package com.classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        Cat anotherCat = new Cat();
+        myCat.name = "Cat 1";
+        myCat.age = 12;
+        System.out.printf("myCat.name : %s \nmyCat.age : %d",myCat.name,myCat.age);
+        anotherCat.name = "Cat 2";
+        anotherCat.age = 14;
+        System.out.printf("\nanotherCat.name : %s \nanotherCat.age : %d",anotherCat.name,anotherCat.age);
+
+    }
+}
