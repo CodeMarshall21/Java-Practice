403ebd21f079042ac3197298c2d12a8073103b41
Keshav <k21keshav05@gmail.com>
2025-02-19 01:28:05 +0530

    Classes

--- CodingWithJohn/CodeWithJohn/src/com/classes/Main.java
+++ CodingWithJohn/CodeWithJohn/src/com/classes/Main.java
@@ -7,9 +7,11 @@ public class Main {
         myCat.name = "Cat 1";
         myCat.age = 12;
         System.out.printf("myCat.name : %s \nmyCat.age : %d",myCat.name,myCat.age);
+        System.out.println("\nmyCat.meow(); -> ");myCat.meow();
         anotherCat.name = "Cat 2";
         anotherCat.age = 14;
         System.out.printf("\nanotherCat.name : %s \nanotherCat.age : %d",anotherCat.name,anotherCat.age);
-
+        System.out.println("\nCat.dingdong() - > ");
+        Cat.dingdong();
     }
 }
