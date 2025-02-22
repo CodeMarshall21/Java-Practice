0226d5d38013f4a0060bad84a3ac13b2f2ca910f
keshav <k21keshav05@gmail.com>
2025-02-23 01:08:10 +0530

    Arrays

--- BroCode/Practice/src/com/A12_Arrays/ArraysBasics.java
+++ BroCode/Practice/src/com/A12_Arrays/ArraysBasics.java
@@ -1,7 +1,24 @@
 package com.A12_Arrays;
 
+import java.util.Arrays;
+
 public class ArraysBasics {
     public static void main(String[] args) {
+        String[] fruits = {"apple", "orange", "banana", "coconut"};
+        String[] fruits2 = new String[5];
 
+        //fruits[0] = "pineapple";
+        //int numOfFruits = fruits.length;
+        Arrays.sort(fruits);
+        Arrays.fill(fruits2, "pineapple");
+
+        for (String fruit : fruits) {
+            System.out.print(fruit + " ");
+
+        }
+        System.out.println("\n");
+        for (String fruit:fruits2){
+            System.out.print(fruit+ " ");
+        }
     }
 }
