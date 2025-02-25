003d594e6dcd563d0ab2f19dcee735324692b1fd
keshav <k21keshav05@gmail.com>
2025-02-26 01:24:51 +0530

    2D-Arrays

--- BroCode/Practice/src/com/A12_Arrays/TwoDimensionalArrays.java
+++ BroCode/Practice/src/com/A12_Arrays/TwoDimensionalArrays.java
@@ -2,6 +2,19 @@ package com.A12_Arrays;
 
 public class TwoDimensionalArrays {
     public static void main(String[] args) {
-        
+        // 2D array = An array where each element is an array
+        // Useful for storing a matrix of data
+        char [][] telephone = {
+                                {'1','2','3'},
+                                {'4','5','6'},
+                                {'7','8','9'},
+                                {'*','0','#'}
+                                               };
+        for (char[] row : telephone){
+            for(char number: row){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
     }
 }
