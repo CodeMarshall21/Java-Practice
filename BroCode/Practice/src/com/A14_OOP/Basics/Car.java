0596a1d030262ab087808494accdc56ce2ad17bf
keshav <k21keshav05@gmail.com>
2025-03-11 00:54:12 +0530

    OOPS Basics

---
+++ BroCode/Practice/src/com/A14_OOP/Basics/Car.java
@@ -0,0 +1,24 @@
+package com.A14_OOP.Basics;
+
+public class Car {
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 1995;
+    boolean isRunning = false;
+
+    void start(){
+        System.out.println("You started the car....");
+        isRunning = true;
+    }
+    void stop(){
+        System.out.println("You STOPPED the car !");
+        isRunning = false;
+    }
+    void drive(){
+        if (isRunning){
+            System.out.println("You are Driving");
+        }else{
+            System.out.println("START THE CAR TO DRIVE !!!");
+        }
+    }
+}
---
+++ BroCode/Practice/src/com/A14_OOP/Basics/Main.java
@@ -0,0 +1,33 @@
+package com.A14_OOP.Basics;
+
+public class Main {
+    public static void main(String[] args) {
+        Car carObj = new Car();
+
+        System.out.println("carObj.make -> "+carObj.make);
+        System.out.println("carObj.model -> "+carObj.model);
+        System.out.println("carObj.year -> "+carObj.year);
+        System.out.println("carObj.isRunning -> "+carObj.isRunning);
+
+        System.out.print("carObj.start(); -> ");
+        carObj.start();
+        System.out.println("carObj.isRunning -> "+carObj.isRunning);
+
+        System.out.print("carObj.stop(); -> ");
+        carObj.stop();
+        System.out.println("carObj.isRunning -> "+carObj.isRunning);
+
+        System.out.print("carObj.drive(); -> ");
+        carObj.drive();
+        System.out.println("carObj.isRunning -> "+carObj.isRunning);
+
+        System.out.print("carObj.start(); -> ");
+        carObj.start();
+        System.out.println("carObj.isRunning -> "+carObj.isRunning);
+
+        System.out.print("carObj.drive(); -> ");
+        carObj.drive();
+        System.out.println("carObj.isRunning -> "+carObj.isRunning);
+
+    }
+}
