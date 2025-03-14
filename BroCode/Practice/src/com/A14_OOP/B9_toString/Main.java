670b288eb925c086d629a0ef0b3f976264756cf2
keshav <k21keshav05@gmail.com>
2025-03-14 23:52:50 +0530

    toString

--- BroCode/Practice/src/com/A14_OOP/B9_toString/Main.java
+++ BroCode/Practice/src/com/A14_OOP/B9_toString/Main.java
@@ -1,4 +1,10 @@
 package com.A14_OOP.B9_toString;
 
 public class Main {
+    public static void main(String[] args) {
+        // .toString() = Method inherited from the Object class.
+        //                       Used to return a string representation of an object.
+        //                       By default, it returns a hash code as a unique identifier.
+        //                       It can be overridden to provide meaningful details.
+    }
 }
