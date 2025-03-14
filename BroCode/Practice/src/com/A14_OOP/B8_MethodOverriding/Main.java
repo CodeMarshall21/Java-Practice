6eac42146425d7e0a814948ecd8b29337a0938cd
keshav <k21keshav05@gmail.com>
2025-03-14 23:45:12 +0530

    Method Overriding

--- BroCode/Practice/src/com/A14_OOP/B8_MethodOverriding/Main.java
+++ BroCode/Practice/src/com/A14_OOP/B8_MethodOverriding/Main.java
@@ -2,6 +2,9 @@ package com.A14_OOP.B8_MethodOverriding;
 
 public class Main {
     public static void main(String[] args) {
+        // Method overriding = When a subclass provides its own
+        //                                      implementation of a method that is already defined.
+        //                                      Allows for code reusability and gives specific implementations.
         Dog dog = new Dog();
         Cat cat = new Cat();
         Fish fish = new Fish();
