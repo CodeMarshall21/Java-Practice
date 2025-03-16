0c9d75bb9380e75db08595e83bfb1d2b76ee7312
keshav <k21keshav05@gmail.com>
2025-03-17 00:24:44 +0530

    Polymorphism

---
+++ BroCode/Practice/src/com/A14_OOP/B12_Polymorphism/Main.java
@@ -0,0 +1,13 @@
+package com.A14_OOP.B12_Polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Polymorphism = "POLY-" = "MANY"
+        //                               "-MORPH" = "SHAPE"
+        //                               Objects can identify as other objects.
+        //                               Objects can be treated as objects of a common superclass.
+
+        
+    }
+}
