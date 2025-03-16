197fbde32a897e5bdc9b83455240e2ba606b6bc9
keshav <k21keshav05@gmail.com>
2025-03-16 22:52:50 +0530

    Abstraction

--- BroCode/Practice/src/com/A14_OOP/B10_Abstraction/Main.java
+++ BroCode/Practice/src/com/A14_OOP/B10_Abstraction/Main.java
@@ -2,6 +2,15 @@ package com.A14_OOP.B10_Abstraction;
 
 public class Main {
     public static void main(String[] args) {
+
+        // abstract = Used to define abstract classes and methods.
+        //                   Abstraction is the process of hiding implementation details
+        //                   and showing only the essential features.
+        //                   Abstract classes CAN'T be instantiated directly.
+        //                   Can contain 'abstract' methods (which must be implemented)
+        //                   Can contain 'concrete' methods (which are inherited)
+
+
         
     }
 }
