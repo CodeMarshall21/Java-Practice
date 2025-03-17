c6ce02b6b60545cfbe9cc1b363109e0554810706
keshav <k21keshav05@gmail.com>
2025-03-17 12:08:34 +0530

    Runtime Polymorphism

--- BroCode/Practice/src/com/A14_OOP/B13_RuntimePolymorphism/Main.java
+++ BroCode/Practice/src/com/A14_OOP/B13_RuntimePolymorphism/Main.java
@@ -2,6 +2,9 @@ package com.A14_OOP.B13_RuntimePolymorphism;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Runtime polymorphism = When the method that gets executed is decided
+        //                                               at runtime based on the actual type of the object.
+
     }
 }
