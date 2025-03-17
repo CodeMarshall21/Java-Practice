bc3e72475985ec0f333152d8316c3f88be1a47eb
keshav <k21keshav05@gmail.com>
2025-03-17 15:58:41 +0530

    Aggrigation

--- BroCode/Practice/src/com/A14_OOP/B15_Aggregation/Main.java
+++ BroCode/Practice/src/com/A14_OOP/B15_Aggregation/Main.java
@@ -2,6 +2,8 @@ package com.A14_OOP.B15_Aggregation;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // Aggregation = Represents a "has-a" relationship between objects.
+        //                           One object contains another object as part of its structure,
+        //                           but the contained object/s can exist independently.
     }
 }
