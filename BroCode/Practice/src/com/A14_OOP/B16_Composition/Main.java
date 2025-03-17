34fd37c747a4de2e4200774b93db0415fe39ca65
keshav <k21keshav05@gmail.com>
2025-03-17 21:09:15 +0530

    Composition

---
+++ BroCode/Practice/src/com/A14_OOP/B16_Composition/Main.java
@@ -0,0 +1,11 @@
+package com.A14_OOP.B16_Composition;
+
+public class Main {
+    public static void main(String[] args) {
+        // Composition = Represents a "part-of" relationship between objects.
+        //                            For example, an Engine is "part of" a Car.
+        //                            Allows complex objects to be constructed from smaller objects.
+
+        
+    }
+}
