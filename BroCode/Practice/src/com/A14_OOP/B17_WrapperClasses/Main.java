0d342f1c6b3821bf4b5f30b5ab5061cedf1ef259
keshav <k21keshav05@gmail.com>
2025-03-19 00:13:17 +0530

    Wrapper Classes

---
+++ BroCode/Practice/src/com/A14_OOP/B17_WrapperClasses/Main.java
@@ -0,0 +1,12 @@
+package com.A14_OOP.B17_WrapperClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        // Wrapper classes = Allow primitive values (int, char, double, boolean)
+        //                                   to be used as objects. "Wrap them in an object"
+        //                                   Generally, don't wrap primitives unless you need an object.
+        //                                   Allows use of Collections Framework and static utility methods.
+
+        
+    }
+}
