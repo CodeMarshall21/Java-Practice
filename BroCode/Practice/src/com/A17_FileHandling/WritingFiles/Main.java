528955b43f59821f3db2eafea0e410e8cff6de79
keshav <k21keshav05@gmail.com>
2025-03-20 00:00:49 +0530

    Writing Files in Java

--- BroCode/Practice/src/com/A17_FileHandling/WritingFiles/Main.java
+++ BroCode/Practice/src/com/A17_FileHandling/WritingFiles/Main.java
@@ -2,6 +2,11 @@ package com.A17_FileHandling.WritingFiles;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // How to write a file using Java (4 popular options)
+
+        // FileWriter = Good for small or medium-sized text files
+        // BufferedWriter = Better performance for large amounts of text
+        // PrintWriter = Best for structured data, like reports or logs
+        // FileOutputStream = Best for binary files (e.g., images, audio files)
     }
 }
