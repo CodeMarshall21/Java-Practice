d24115b166a617fd2d6459830285679631bcdaa2
keshav <k21keshav05@gmail.com>
2025-03-20 11:31:55 +0530

    Hash And Stars

--- BoardInfinity/src/com/StringManipulation/HashAndStar.java
+++ BoardInfinity/src/com/StringManipulation/HashAndStar.java
@@ -3,5 +3,48 @@ package com.StringManipulation;
 public class HashAndStar {
     public static void main(String[] args) {
 
+        // print if (max(starCount,hashCount) < 3) -> "+" * max(starCount,hashCount) + "*" * max(starCount,hashCount)
+        // else -> "+" * min(starCount,hashCount) + "*" * min(starCount,hashCount)
+
+        String str = "##****";
+        int starCount = 0;
+        int hashCount = 0;
+
+
+        for (char chr: str.toCharArray()){
+            if (chr == '#'){
+                hashCount ++;
+            }
+            if (chr == '*'){
+                starCount ++;
+            }
+
+        }
+
+        String newStr = "";
+
+        int maxCount = Math.max(hashCount,starCount);
+        int minCount = Math.min(hashCount,starCount);
+
+
+        if (maxCount <= 3){
+            for (int i = 0; i < maxCount; i++) {
+                newStr += "#";
+            }
+            for (int i = 0; i < maxCount; i++) {
+                newStr += "*";
+            }
+        }else{
+            for (int i = 0; i < minCount; i++) {
+                newStr += "#";
+            }
+            for (int i = 0; i < minCount; i++) {
+                newStr += "*";
+            }
+        }
+
+
+
+        System.out.println(newStr);
     }
 }
