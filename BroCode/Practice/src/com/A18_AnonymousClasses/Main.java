8a021a51e5f4222aca53a052be6126b7e5c031f4
keshav <k21keshav05@gmail.com>
2025-03-26 23:57:35 +0530

    Anonymous Class

---
+++ BroCode/Practice/src/com/A18_AnonymousClasses/Main.java
@@ -0,0 +1,9 @@
+package com.A18_AnonymousClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        // Anonymous class = A class that doesn't have a name. Cannot be reused.
+        //                                     Add custom behavior without having to create a new class.
+        //                                     Often used for one time uses (TimerTask, Runnable, callbacks)
+    }
+}
