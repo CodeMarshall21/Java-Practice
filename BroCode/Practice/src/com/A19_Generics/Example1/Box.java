1804f930a81473b07bce1b0586dca7e630ba2457
keshav <k21keshav05@gmail.com>
2025-03-28 23:31:23 +0530

    Generics Example 1

--- BroCode/Practice/src/com/A19_Generics/Example1/Box.java
+++ BroCode/Practice/src/com/A19_Generics/Example1/Box.java
@@ -1,4 +1,15 @@
 package com.A19_Generics.Example1;
 
-public class Box {
+public class Box <T>{ //    <T> <- refers type (or it can be anything inside diamond operator)
+
+    T item;     //      <- initialize variable "item" of 'T' data type
+
+    public void setItem(T item){
+        this.item = item;
+    }
+
+    public void getItem(){
+        System.out.println(this.item);
+    }
+
 }
--- BroCode/Practice/src/com/A19_Generics/Example1/Main.java
+++ BroCode/Practice/src/com/A19_Generics/Example1/Main.java
@@ -8,6 +8,15 @@ public class Main {
         //                     type parameter (placeholder that gets replaced with a real type)
         //                     type argument (specifies the type)
 
-            
+        Box<String> box1 = new Box<>();
+        box1.setItem("Banana");
+        box1.getItem();
+
+        Box<Integer> quantity = new Box<>();
+        quantity.setItem(69);
+        quantity.getItem();
+
+
+
     }
 }
