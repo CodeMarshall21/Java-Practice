1036c8cb37a0f4d5d5b2ef067047898489fb6f25
keshav <k21keshav05@gmail.com>
2025-03-28 23:08:48 +0530

    Hashmap

--- BroCode/Practice/src/com/A19_Hashmap/Main.java
+++ BroCode/Practice/src/com/A19_Hashmap/Main.java
@@ -5,6 +5,6 @@ public class Main {
         // HashMap = A data structure that stores key-value pairs
         //                      Keys are unique, but Values can be duplicated
         //                      Does not maintain any order, but is memory efficient
-        
+        //                      HashMap<Key, Value>
     }
 }
