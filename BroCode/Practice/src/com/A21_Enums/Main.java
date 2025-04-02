2e1075bccea23e3323b685196a68a3d3e280abc6
keshav <k21keshav05@gmail.com>
2025-04-02 06:21:43 +0530

    Enums

--- BroCode/Practice/src/com/A21_Enums/Main.java
+++ BroCode/Practice/src/com/A21_Enums/Main.java
@@ -1,11 +1,51 @@
 package com.A21_Enums;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //           Enums = (Enumerations) A special kind of class that
 //                   represents a fixed set of constants.
 //                   They improve code readability and reliability.
 //                   More efficient with switches than Strings.
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a day of the week: ");
+        String input = in.nextLine().toUpperCase();
+
+
+        try{
+            Days day = Days.valueOf(input);
+
+            switch(day){
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FRIDAY -> System.out.println("It is a weekday");
+                case SATURDAY, SUNDAY -> System.out.println("It is the weekend");
+            }
+        }catch(IllegalArgumentException e){
+            System.out.println("INVALID INPUT");
+        }
+
+
+        System.out.println("All days of the week :- ");
+
+        for(Days d : Days.values()){
+            System.out.println("Day : "+d+" "+d+".ordinal() -> "+d.ordinal());
+        }
+        System.out.println("\n");
+        for(Days d : Days.values()){
+            System.out.println("Day : "+d+" "+d+".getDay() -> "+d.getDay());
+        }
+
+
+
+
+
+
 
 
     }
