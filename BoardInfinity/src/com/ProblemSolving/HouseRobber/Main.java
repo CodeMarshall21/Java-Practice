85251756cb65d5523ad2b1a5787423bacabef738
keshav <k21keshav05@gmail.com>
2025-04-02 11:45:14 +0530

    Initial Commit

---
+++ BoardInfinity/src/com/ProblemSolving/HouseRobber/Main.java
@@ -0,0 +1,28 @@
+package com.ProblemSolving.HouseRobber;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] houses = {2,7,9,3,1};
+        System.out.println("Houses: "+ Arrays.toString(houses));
+
+        int totalProfit = robber(houses);
+        System.out.println("Total Profit: "+totalProfit);
+    }
+
+    public static int robber(int[] houses){
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for (int i = 0; i < houses.length; i=i+2) {
+            evenSum += houses[i];
+        }
+
+        for (int i = 1; i < houses.length; i=i+2) {
+            oddSum += houses[i];
+        }
+        return Math.max(oddSum,evenSum);
+
+    }
+}
