a26ecf2f06fa23d29d20469984082bdae0670b0e
keshav <k21keshav05@gmail.com>
2025-04-05 23:31:52 +0530

    LocalDate

--- BroCode/Practice/src/com/A24_DateAndTime/Main.java
+++ BroCode/Practice/src/com/A24_DateAndTime/Main.java
@@ -1,8 +1,13 @@
 package com.A24_DateAndTime;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         // How to work with DATES & TIMES using Java
         // (LocalDate, LocalTime, LocalDateTime, UTC timestamp)
+
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
     }
 }
