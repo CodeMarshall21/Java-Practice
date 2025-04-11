ce6e5f55f182f5acde02f468398544c3ccfe9d39
keshav <k21keshav05@gmail.com>
2025-04-12 01:13:51 +0530

    Insertion Sort

---
+++ BroCode/Data Structures/src/com/A12_InsertionSort/InsertionSort.java
@@ -0,0 +1,46 @@
+package com.A12_InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        // Insertion sort = after comparing elements to the left,
+        //				shift elements to the right to make room to insert a value
+
+        //				Quadratic time O(n^2)
+        //				small data set = decent
+        //				large data set = BAD
+
+        //				Less steps than Bubble sort
+        //				Best case is O(n) compared to Selection sort O(n^2)
+
+        int [] array = {9, 1, 8, 2, 7, 3, 6, 5, 4};
+
+        System.out.print("Array: ");
+        for (int val: array){
+            System.out.print(val + " ");
+        }
+
+        System.out.println("\nSorting...");
+
+        insertionSort(array);
+
+        System.out.print("Sorted Array: ");
+        for (int val: array){
+            System.out.print(val + " ");
+        }
+    }
+
+    public static void insertionSort(int[] array){
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while(j >= 0 && temp < array[j]){
+                array[j+1] = array[j];
+                j-- ;
+            }
+            array[j+1] = temp;
+            System.out.println("Array after Iteration "+i+" : "+ Arrays.toString(array));
+        }
+    }
+}
