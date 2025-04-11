d06232426bb82cc8eea452fe4b4cf0f992742a6b
keshav <k21keshav05@gmail.com>
2025-04-11 23:11:20 +0530

    Bubble Sort

---
+++ BroCode/Data Structures/src/com/A10_BubbleSort/BubbleSort.java
@@ -0,0 +1,45 @@
+package com.A10_BubbleSort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        // bubble sort = pairs of adjacent elements are compared, and the elements
+        //		            swapped if they are not in order.
+
+        //				 Quadratic time O(n^2)
+        //				 small data set = okay-ish
+        //				 large data set = BAD (plz don't)
+
+
+        int [] array = {2,5,6,4,8,9,7,1,0,3};
+
+
+        System.out.print("Array: ");
+        for (int val : array){
+            System.out.print(val+" ");
+        }
+
+        System.out.println("\nSorting....");
+
+        bubbleSort(array);
+
+        System.out.print("Sorted Array: ");
+        for (int val : array){
+            System.out.print(val+" ");
+        }
+
+
+
+    }
+
+    public static void bubbleSort(int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+}
