55a0d7536cab5bdea30f92c809ae14264606156b
keshav <k21keshav05@gmail.com>
2025-04-13 02:21:04 +0530

    Quick sort

--- BroCode/Data Structures/src/com/A15_QuickSort/QuickSort.java
+++ BroCode/Data Structures/src/com/A15_QuickSort/QuickSort.java
@@ -20,17 +20,52 @@ public class QuickSort {
 
         System.out.println("\nSorting...");
 
-        quickSort(array);
+        long start = System.nanoTime();
+        quickSort(array,0,array.length - 1);
+        long stop = System.nanoTime();
+        long elapse = stop - start;
+
 
         System.out.print("Sorted Array: ");
         for(int val :array){
             System.out.print(val + " ");
         }
 
+        System.out.println("\nTime Taken: "+elapse+" ns");
+
     }
 
-    private static void quickSort(int[] array) {
+    private static void quickSort(int[] array, int start, int end) {
+        if (end <= start) {
+            return ;
+        }
 
+        int pivot = partition(array,start,end);
+        quickSort(array,start,pivot - 1);
+        quickSort(array,pivot + 1,end);
+
+
+    }
+
+    private static int partition(int[] array, int start, int end) {
+
+        int i = start - 1;
+        int pivot = array[end];
+
+        for (int j = start; j <= end; j++) {
+            if (array[j] < pivot){
+                i ++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i ++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+
+        return i;
     }
 
 
