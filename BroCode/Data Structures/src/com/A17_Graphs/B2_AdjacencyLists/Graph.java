2a089fb247355ed7427d2cacd66c9c3aa028c73d
keshav <k21keshav05@gmail.com>
2025-04-21 02:44:24 +0530

    Adjacency Lists Built Graph & Node Logic

--- BroCode/Data Structures/src/com/A17_Graphs/B2_AdjacencyLists/Graph.java
+++ BroCode/Data Structures/src/com/A17_Graphs/B2_AdjacencyLists/Graph.java
@@ -3,6 +3,48 @@ package com.A17_Graphs.B2_AdjacencyLists;
 import java.util.*;
 
 public class Graph {
-    Node node;
     ArrayList<LinkedList<Node>> adjList;
+
+    Graph(){
+        adjList = new ArrayList<>();
+    }
+
+    public void addNode(Node node){
+        LinkedList<Node> currentList = new LinkedList<>();
+        currentList.add(node);
+        adjList.add(currentList);
+    }
+
+    public void addEdge(int src, int dst){
+        LinkedList<Node> currentList = adjList.get(src);
+        Node dstNode = adjList.get(dst).get(0);
+        currentList.add(dstNode);
+
+//        Or could be replaced with :-
+//        Node dstNode = adjList.get(dst).get(0);
+//        adjList.get(src).add(dstNode);
+    }
+
+    public boolean checkEge(int src, int dst){
+        LinkedList<Node> currentList = adjList.get(src);
+        Node dstNode = adjList.get(dst).get(0);
+
+        for(Node node: currentList){
+            if (node == dstNode){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print(){
+        for(LinkedList<Node> lists: adjList){
+            for(Node node: lists){
+                System.out.print(node.data+" -> ");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
