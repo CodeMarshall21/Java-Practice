67bf057d33ae7867d1962019c352f90716af327a
keshav <k21keshav05@gmail.com>
2025-07-15 18:44:22 +0530

    1672. Richest Customer Wealth

---
+++ Kunal/Questions/src/com/Q1_LinearSearch/Leetcode1672/Solution.java
@@ -0,0 +1,26 @@
+package com.Q1_LinearSearch.Leetcode1672;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,5},
+                {7,3},
+                {3,5}
+        };
+
+        System.out.println(maximumWealth(arr));
+    }
+    static int maximumWealth(int[][] accounts) {
+        int maxWealth = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++){
+            int wealthSum = 0;
+            for (int account = 0; account < accounts[person].length; account++){
+                wealthSum += accounts[person][account];
+            }
+            if(wealthSum > maxWealth){
+                maxWealth = wealthSum;
+            }
+        }
+        return maxWealth;
+    }
+}
