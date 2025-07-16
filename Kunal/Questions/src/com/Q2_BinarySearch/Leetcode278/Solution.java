28f267dbaeb9db743ab09341b668a6f3ecdb8b2d
keshav <k21keshav05@gmail.com>
2025-07-17 01:12:11 +0530

    278. First Bad Version

--- Kunal/Questions/src/com/Q2_BinarySearch/Leetcode278/Solution.java
+++ Kunal/Questions/src/com/Q2_BinarySearch/Leetcode278/Solution.java
@@ -1,7 +1,43 @@
 package com.Q2_BinarySearch.Leetcode278;
 
-public class Solution {
+public class Solution extends VersionControl{
+
+    public Solution(int badVersion){
+        super(badVersion);
+    }
+
     public static void main(String[] args) {
-        
+        Solution solution = new Solution (4);
+        System.out.println("Then " + solution.firstBadVersion(5) + " is the first bad version.");
+    }
+
+    public int firstBadVersion(int upperBound) {
+
+        int left = 1;
+        int right = upperBound;
+
+        int mid = 0;
+
+        if(left == right){
+            return 1;
+        }
+
+        boolean result;
+
+        while (left < right){
+            mid = left + (right - left) / 2;
+            result = isBadVersion(mid);
+
+            if (result){
+                right = mid;
+            }
+            if(!result){
+                left = mid + 1;
+            }
+            System.out.printf("call isBadVersion(%d) -> %b\n",mid,result);
+
+        }
+
+        return left;
     }
 }
