37131ad438267d66eec31b6ffb93cfd373c38395
keshav <k21keshav05@gmail.com>
2025-07-16 23:58:51 +0530

    374. Guess Number Higher or Lower

---
+++ Kunal/Questions/src/com/Q2_BinarySearch/Leetcode374/GuessGame.java
@@ -0,0 +1,14 @@
+package com.Q2_BinarySearch.Leetcode374;
+
+public class GuessGame {
+    int guessVal;
+
+    public GuessGame(int guessVal){
+        this.guessVal = guessVal;
+    }
+
+    public int guess(int num){
+
+        
+    }
+}
--- Kunal/Questions/src/com/Q2_BinarySearch/Leetcode374/Solution.java
+++ Kunal/Questions/src/com/Q2_BinarySearch/Leetcode374/Solution.java
@@ -1,7 +1,29 @@
 package com.Q2_BinarySearch.Leetcode374;
 
-public class Solution {
+public class Solution extends GuessGame {
     public static void main(String[] args) {
 
+    }
+
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            int outcome = guess(mid);
+
+            if (outcome == 0){
+                return mid;
+            }
+            else if(outcome == 1){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+
+        }
+        return -1;
     }
 }
