14eea939bea3cd9508acdae3640ec590041bf950
keshav <k21keshav05@gmail.com>
2025-07-19 01:12:40 +0530

    Order Agnostic Binary Search - Prompting

--- Kunal/Practise/src/com/A6_BinarySearch/OrderAgnosticBS.java
+++ Kunal/Practise/src/com/A6_BinarySearch/OrderAgnosticBS.java
@@ -2,8 +2,10 @@ package com.A6_BinarySearch;
 
 public class OrderAgnosticBS {
     public static void main(String[] args) {
-        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
-        int target = 22;
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+
+        int target = 80;
 
         int pos = orderAgnosticBS(arr,target);
         System.out.printf("Element at: %d",pos);
@@ -12,9 +14,14 @@ public class OrderAgnosticBS {
         int start = 0;
         int end = arr.length - 1;
         boolean isAsc = arr[start] < arr[end];
+        int count = 0;
+
+        System.out.println("Array is in ascending order: "+isAsc);
 
         while(start <= end){
             int mid = start + (end - start) / 2;
+            System.out.printf("iteration %d : ",++count);
+            System.out.println("Current mid => "+ mid);
 
             if(arr[mid] == target){
                 return mid;
