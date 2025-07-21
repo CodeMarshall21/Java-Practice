a71f0ed1cf4032c937a0298689b5035ddb955885
keshav <k21keshav05@gmail.com>
2025-07-21 23:52:29 +0530

    Binary Search - Searching in stricly sorted array

---
+++ Kunal/Practise/src/com/A6_BinarySearch/StrictlySortedMatrix.java
@@ -0,0 +1,15 @@
+package com.A6_BinarySearch;
+
+public class StrictlySortedMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] colBinarySearch (int[][] matrix, int row, int cStart, int cEnd, int target){
+        return new int[] {-1,-1};
+    }
+
+    static int[] search (int [][] matrix, int target){
+        
+    }
+}
