e450b2d129d72dcc0f930a4b8b8a5c2d8eed3451
keshav <k21keshav05@gmail.com>
2025-07-23 02:32:34 +0530

    Bubble Sort - Optimised

--- Kunal/Practise/src/com/A7_BubbleSort/BubbleSort.java
+++ Kunal/Practise/src/com/A7_BubbleSort/BubbleSort.java
@@ -4,18 +4,25 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int [] arr = {5,3,6,9,4,1};
+        int [] arr = {1,2,3,4,5,6};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void bubbleSort (int[]arr){
+        boolean swapped;
+
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if(arr[j] < arr[j - 1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped){
+                break;
             }
         }
     }
