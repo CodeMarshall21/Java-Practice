7cfa4b782a9bb797210a60b990c76f7ec553ecce
keshav <k21keshav05@gmail.com>
2025-07-23 02:34:26 +0530

    Bubble Sort - Added Comments

--- Kunal/Practise/src/com/A7_BubbleSort/BubbleSort.java
+++ Kunal/Practise/src/com/A7_BubbleSort/BubbleSort.java
@@ -11,16 +11,21 @@ public class BubbleSort {
     static void bubbleSort (int[]arr){
         boolean swapped;
 
-        for (int i = 0; i < arr.length; i++) {
+        // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {  // Counter value
             swapped = false;
+            // for each step, max item will come at the last respective index
             for (int j = 1; j < arr.length - i; j++) {
+                // swap if the item is smaller than the previous item
                 if(arr[j] < arr[j - 1]){
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                     swapped = true;
                 }
             }
+            // if you did not swap for a particular value of i, it means the array is sorted hence stop the program
             if (!swapped){
                 break;
             }
