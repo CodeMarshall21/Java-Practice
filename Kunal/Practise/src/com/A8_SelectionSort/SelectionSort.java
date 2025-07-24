9e3eb517af22032c26bcbc11d6eb8be4eee93e67
keshav <k21keshav05@gmail.com>
2025-07-24 20:09:38 +0530

    Selection Sort

--- Kunal/Practise/src/com/A8_SelectionSort/SelectionSort.java
+++ Kunal/Practise/src/com/A8_SelectionSort/SelectionSort.java
@@ -1,15 +1,18 @@
 package com.A8_SelectionSort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
-
+        int[] arr = {-65,-89,0,-21,5,1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
     static void selectionSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             int lastIndex = arr.length - i - 1;
             int maxValIndex = maxVal(arr, 0, lastIndex);
             swapNums(arr, maxValIndex, lastIndex);
-
         }
     }
 
@@ -21,7 +24,7 @@ public class SelectionSort {
 
     static int maxVal(int[] arr, int start, int end) {
         int max = start;
-        for (int i = start; i < end; i++) {
+        for (int i = start; i <= end; i++) {
             if (arr[i] > arr[max]){
                 max = i;
             }
