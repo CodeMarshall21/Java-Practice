74eeffd95210a709ad8db5dff42654d2f3bffa09
keshav <k21keshav05@gmail.com>
2025-07-26 00:21:47 +0530

    Cyclic Sort

---
+++ Kunal/Practise/src/com/A10_CyclicSort/CyclicSort.java
@@ -0,0 +1,29 @@
+package com.A10_CyclicSort;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            }
+            else{
+                i ++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
