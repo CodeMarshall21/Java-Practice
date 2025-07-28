06b21d9fbbf155afdcafd3e63080b9522bb1ee4c
keshav <k21keshav05@gmail.com>
2025-07-28 19:11:05 +0530

    287. Find the Duplicate Number

--- Kunal/Questions/src/com/Q2_Sort/Q1_CyclicSort/Leetcode287/Solution.java
+++ Kunal/Questions/src/com/Q2_Sort/Q1_CyclicSort/Leetcode287/Solution.java
@@ -4,7 +4,30 @@ class Solution {
     public static void main(String[] args) {
 
     }
-    public int findDuplicate(int[] nums) {
-        
+    static int findDuplicate(int[] nums) {
+        int i = 0;
+
+        while(i < nums.length){
+            if(nums[i] != i + 1){
+                int correctIndex = nums[i] - 1;
+                if(nums[i] != nums[correctIndex]){
+                    swapNum(nums,i, correctIndex);
+                }
+                else{
+                    return nums[i];
+                }
+            }
+            else{
+                i++;
+            }
+
+        }
+        return -1;
+    }
+
+    static void swapNum(int[] nums, int i, int correctIndex) {
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
     }
 }
