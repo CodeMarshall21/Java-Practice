5502abd8b7d0a1bd46f904dfc24637e9aabebce2
keshav <k21keshav05@gmail.com>
2025-07-28 23:16:03 +0530

    645. Set Mismatch

--- Kunal/Questions/src/com/Q2_Sort/Q1_CyclicSort/Leetcode645/Solution.java
+++ Kunal/Questions/src/com/Q2_Sort/Q1_CyclicSort/Leetcode645/Solution.java
@@ -1,13 +1,39 @@
 package com.Q2_Sort.Q1_CyclicSort.Leetcode645;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 class Solution {
     public static void main(String[] args) {
-
+        int[] nums = {1,1};
+        System.out.println(Arrays.toString(findErrorNums(nums)));
     }
-    public int[] findErrorNums(int[] nums) {
+    static int[] findErrorNums(int[] nums) {
         int i = 0;
+        int[] ans = new int[2];
+
         while(i < nums.length){
-            
+            int correctIndex = nums[i] - 1;
+            if(nums[i] != nums[correctIndex]){
+                swapNums(nums, i, correctIndex);
+            }
+            else{
+                i++;
+            }
         }
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j] != j+1){
+                ans[0] = nums[j];
+                ans[1] = j+1;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    static void swapNums(int[] nums, int i, int correctIndex) {
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
     }
 }
