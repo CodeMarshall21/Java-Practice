e0465e5c8c2cd34fb126d9b29ebde04dc8f66f52
keshav <k21keshav05@gmail.com>
2025-07-28 23:08:57 +0530

    645. Set Mismatch

---
+++ Kunal/Questions/src/com/Q2_Sort/Q1_CyclicSort/Leetcode645/Solution.java
@@ -0,0 +1,13 @@
+package com.Q2_Sort.Q1_CyclicSort.Leetcode645;
+
+class Solution {
+    public static void main(String[] args) {
+
+    }
+    public int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            
+        }
+    }
+}
