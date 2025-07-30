4287a1271d09f9acaf879f8ccd419ac91aa6a7e0
keshav <k21keshav05@gmail.com>
2025-07-31 01:15:56 +0530

    Strings and StringBuilder - Performance

--- Kunal/Practise/src/com/A11_StringsAndStringBuilder/Performance.java
+++ Kunal/Practise/src/com/A11_StringsAndStringBuilder/Performance.java
@@ -2,6 +2,17 @@ package com.A11_StringsAndStringBuilder;
 
 public class Performance {
     public static void main(String[] args) {
+        System.out.println("\"a\" + \"b\" = \"ab\"   <- \"ab\" is a whole new object");
 
+        String series = "";
+        for (int i = 0; i < 26; i++) {
+            series += (char)('a'+i);
+        }
+        System.out.print("series -> ");
+        System.out.println(series);
+
+        // The above example of wastage of space and inefficient method,
+        // were there is a new object created every time a character is concatenated.
+        // So there are 25 other objects without any reference variable
     }
 }
