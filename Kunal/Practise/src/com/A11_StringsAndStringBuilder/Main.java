43c3250668129f854d9c56dfb963b43af271b4db
keshav <k21keshav05@gmail.com>
2025-07-30 23:58:44 +0530

    Strings and StringBuilder

--- Kunal/Practise/src/com/A11_StringsAndStringBuilder/Main.java
+++ Kunal/Practise/src/com/A11_StringsAndStringBuilder/Main.java
@@ -2,6 +2,15 @@ package com.A11_StringsAndStringBuilder;
 
 public class Main {
     public static void main(String[] args) {
+        String a = "Keshav";
+        String b = "Keshav";
+
+        System.out.println(a==b); // a and b referencing same object
+
+        String first = new String("Keshav1");
+        String second = new String("Keshav1");
+
+        System.out.println(first == second); // first and second referencing different objects
 
     }
 }
--- Kunal/Practise/src/com/A7_BubbleSort/BubbleSort.java
+++ Kunal/Practise/src/com/A7_BubbleSort/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6};
+        int [] arr = {6,5,4,3,2,1};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
