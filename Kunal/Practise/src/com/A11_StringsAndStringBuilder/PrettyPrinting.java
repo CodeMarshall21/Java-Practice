b035e04f88c5c21b86e8afdf83acadac15db4c9e
keshav <k21keshav05@gmail.com>
2025-07-31 00:35:34 +0530

    Strings and StringBuilder - Pretty Print

--- Kunal/Practise/src/com/A11_StringsAndStringBuilder/PrettyPrinting.java
+++ Kunal/Practise/src/com/A11_StringsAndStringBuilder/PrettyPrinting.java
@@ -8,5 +8,19 @@ public class PrettyPrinting {
         System.out.printf("Pie: %.3f\n", Math.PI);
 
         System.out.printf("Hello my name is %s and I am %s\n", "Keshav", "Cool");
+
+//        There are many format specifiers we can use. Here are some common ones:
+//
+//                %c - Character
+//                %d - Decimal number (base 10)
+//                %e - Exponential floating-point number
+//                %f - Floating-point number
+//                %i - Integer (base 10)
+//                %o - Octal number (base 8)
+//                %s - String
+//                %u - Unsigned decimal (integer) number
+//                %x - Hexadecimal number (base 16)
+//                %t - Date/time
+//                %n - Newline
     }
 }
