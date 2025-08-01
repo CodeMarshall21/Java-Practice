a860da43a7172824475054c287f934699194af7b
keshav <k21keshav05@gmail.com>
2025-08-01 22:37:20 +0530

    StringBuilder - Intro

--- Kunal/Practise/src/com/A11_StringsAndStringBuilder/StringBuilderPractice.java
+++ Kunal/Practise/src/com/A11_StringsAndStringBuilder/StringBuilderPractice.java
@@ -2,6 +2,13 @@ package com.A11_StringsAndStringBuilder;
 
 public class StringBuilderPractice {
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
 
+        for (int i = 0; i < 26; i++) {
+            char letter = (char)('a' + i);
+            stringBuilder.append(letter);   // No new objects created everytime
+        }
+
+        System.out.println(stringBuilder);  // Try out many functions in StringBuilder class using it's object
     }
 }
