6de0e6c6fef6cd7fd893cbae76f52577001a854d
keshav <k21keshav05@gmail.com>
2025-08-02 22:20:18 +0530

    Patterns - Pattern 1

--- Kunal/Practise/src/com/A12_Patterns/Main.java
+++ Kunal/Practise/src/com/A12_Patterns/Main.java
@@ -2,13 +2,27 @@ package com.A12_Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern2(5);
+        pattern1(5);
     }
+
+    static void pattern1 (int n){
+        // number of lines tbe pattern = number of rows => NUMBER OF TIMES THE OUTER LOOP WILL RUN
+        for (int rows = 1; rows <= n ; rows++) {
+            // Identify for each row how many columns are there
+            for (int columns = 1; columns <= n; columns++) {
+                // What do you need to print => here, "*"
+                System.out.print("* ");
+            }
+            // After filling a row with required columns, add a new line
+            System.out.println();
+        }
+    }
+
     static void pattern2 (int n){
         // number of lines tbe pattern = number of rows => NUMBER OF TIMES THE OUTER LOOP WILL RUN
-        for (int rows = 0; rows <= n-1 ; rows++) {
+        for (int rows = 1; rows <= n ; rows++) {
             // Identify for each row how many columns are there
-            for (int columns = 0; columns <= rows; columns++) {
+            for (int columns = 1; columns <= rows; columns++) {
                 // What do you need to print => here, "*"
                 System.out.print("* ");
             }
