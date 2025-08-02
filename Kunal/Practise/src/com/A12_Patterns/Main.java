24d441b8c80fcae3f755cd00e5f072332c49a0ff
keshav <k21keshav05@gmail.com>
2025-08-02 22:18:48 +0530

    Patterns - Pattern 2

--- Kunal/Practise/src/com/A12_Patterns/Main.java
+++ Kunal/Practise/src/com/A12_Patterns/Main.java
@@ -5,7 +5,7 @@ public class Main {
         pattern2(5);
     }
     static void pattern2 (int n){
-        // number of lines tbe pattern = number of rows => NUMBER OF TIMIES THE OUTTER LOOP WILL RUN
+        // number of lines tbe pattern = number of rows => NUMBER OF TIMES THE OUTER LOOP WILL RUN
         for (int rows = 0; rows <= n-1 ; rows++) {
             // Identify for each row how many columns are there
             for (int columns = 0; columns <= rows; columns++) {
