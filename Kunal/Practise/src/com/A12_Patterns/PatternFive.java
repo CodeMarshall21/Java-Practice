8126ff0c412f3d5945efe4a4e8e82f7dca7de131
keshav <k21keshav05@gmail.com>
2025-08-02 23:36:51 +0530

    Patterns - Pattern 5

--- Kunal/Practise/src/com/A12_Patterns/PatternFive.java
+++ Kunal/Practise/src/com/A12_Patterns/PatternFive.java
@@ -2,10 +2,23 @@ package com.A12_Patterns;
 
 public class PatternFive {
     public static void main(String[] args) {
-        pattern5(5);
+        patter5optimized(5);
     }
 
-    
+    static void patter5optimized(int n){
+        for (int rows = 1; rows <= 2 * n - 1; rows++) {
+
+            // Number of column in each row dependent on 'rows' value and 'n'
+            // After few rows, find how many should we need to remove in each row
+            // Subtract that number from 'n'
+
+            int requiredColumns = rows <= n ? rows : 2 * n - rows;   // n - (rows - n) => 2 * n - rows
+            for (int columns = 1; columns <= requiredColumns; columns++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 
     static void pattern5 (int n){
         // number of lines tbe pattern = number of rows => NUMBER OF TIMES THE OUTER LOOP WILL RUN
