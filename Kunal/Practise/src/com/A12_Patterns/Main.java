7d66954f3470e4a676b0fa1e301ee605c46a0f1a
keshav <k21keshav05@gmail.com>
2025-08-03 20:42:01 +0530

    Patterns - Pattern 28

--- Kunal/Practise/src/com/A12_Patterns/Main.java
+++ Kunal/Practise/src/com/A12_Patterns/Main.java
@@ -2,7 +2,7 @@ package com.A12_Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern4(5);
+        pattern28(5);
     }
 
     static void pattern1 (int n){
@@ -96,6 +96,23 @@ public class Main {
                 }
                 System.out.println();
             }
+        }
+    }
+
+    static void pattern28(int n){
+        for (int rows = 1; rows <= 2 * n - 1; rows++){
+
+            int requiredColumns = rows <= n ? rows : 2 * n - rows;
+            int requiredSpaces = n - requiredColumns;
+
+            for (int spaces = 0; spaces < requiredSpaces; spaces++) {
+                System.out.print(" ");
+            }
+
+            for (int columns = 1; columns <= requiredColumns; columns++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
