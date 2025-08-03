9d7aae36e66d9ed0a231f70d327b61b843769306
keshav <k21keshav05@gmail.com>
2025-08-03 22:45:15 +0530

    Patterns - Pattern 12

--- Kunal/Practise/src/com/A12_Patterns/Practise.java
+++ Kunal/Practise/src/com/A12_Patterns/Practise.java
@@ -2,7 +2,7 @@ package com.A12_Patterns;
 
 public class Practise {
     public static void main(String[] args) {
-        pattern11(5);
+        pattern12(5);
     }
     static void pattern6(int n){
         for (int rows = 1; rows <= n; rows++) {
@@ -55,6 +55,22 @@ public class Practise {
                 System.out.print(" ");
             }
             for (int column = 1; column <= n - (rows - 1); column++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static void pattern12(int n){
+        for (int rows = 1; rows <= 2 * n; rows++) {
+            int requiredColumns = rows > n ? rows - n : n - (rows - 1);
+            int requiredSpaces = rows > n ? 2 * n - rows : rows - 1;
+
+            for (int spaces = 1; spaces <= requiredSpaces ; spaces++) {
+                System.out.print(" ");
+            }
+
+            for (int columns = 1; columns <= requiredColumns ; columns++) {
                 System.out.print("* ");
             }
             System.out.println();
