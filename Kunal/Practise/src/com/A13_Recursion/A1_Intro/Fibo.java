e5fefde73b44da10ec2b0bd5895ccdf06d3fc99e
keshav <k21keshav05@gmail.com>
2025-08-14 16:32:41 +0530

    Recursion - Fibonacci

---
+++ Kunal/Practise/src/com/A13_Recursion/A1_Intro/Fibo.java
@@ -0,0 +1,22 @@
+package com.A13_Recursion.A1_Intro;
+
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter nth Term: ");
+        int term = in.nextInt();
+//        System.out.println(term);
+
+        System.out.printf("%dth term is : %d",term,fibo(term));
+    }
+
+    static int fibo(int n) {
+        // base condition
+        if (n < 2) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
