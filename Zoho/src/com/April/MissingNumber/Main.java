6d0f88a31987231b3646e629a84663efe2a12b91
keshav <k21keshav05@gmail.com>
2025-09-04 01:27:16 +0530

    Zoho - Missing Number

--- Zoho/src/com/April/MissingNumber/Main.java
+++ Zoho/src/com/April/MissingNumber/Main.java
@@ -2,6 +2,34 @@ package com.April.MissingNumber;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int[] arr = {2, 0, 3, 4, 6, 1};
+        System.out.println(missingNumber(arr));
+    }
+
+    private static int missingNumber(int[] arr){
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != i){
+                return i;
+            }
+        }
+        return arr.length;
+    }
+
+    private static void sort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] < arr.length && arr[i] != i){
+                swap(arr, arr[i], i);
+            }else{
+                i++;
+            }
+        }
+    }
+
+    private static void swap (int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 }
