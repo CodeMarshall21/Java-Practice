2e19a5eed839c10f463505facb3cff1bec1e75c0
keshav <k21keshav05@gmail.com>
2025-09-04 01:49:57 +0530

    Zoho - Missing Number

---
+++ Zoho/src/com/April/MissingNumber/SecondMethod.java
@@ -0,0 +1,25 @@
+package com.April.MissingNumber;
+
+public class SecondMethod {
+    public static void main(String[] args) {
+        int[] arr = {0,1};
+        System.out.println(missingNumber(arr));
+    }
+
+    private static int missingNumber(int[] arr){
+        int missingNumber = 0;
+        int originalSum = 0;
+        int arrSum = 0;
+
+        for (int i = 0; i < arr.length + 1; i++) {
+            originalSum += i;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arrSum += arr[i];
+        }
+
+        missingNumber = originalSum - arrSum;
+        return missingNumber;
+    }
+}
