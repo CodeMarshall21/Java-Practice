8dd1c11e5bd9037cd11b3811ae46e9f719a5f0c4
keshav <k21keshav05@gmail.com>
2025-09-04 01:33:10 +0530

    Zoho - Missing Number

--- Zoho/src/com/April/MissingNumber/Main.java
+++ Zoho/src/com/April/MissingNumber/Main.java
@@ -1,5 +1,13 @@
 package com.April.MissingNumber;
+/*
+Find the Missing Number from 0 to n distinct numbers
 
+logic:-
+    1. use cyclic sort to sort numbers from 0 - n
+    2. any number not in correct position, the number which should've been in that position is the missing number
+    3. if every number is in correct position, then the length of the arr is the missing number
+
+*/
 public class Main {
     public static void main(String[] args) {
         int[] arr = {2, 0, 3, 4, 6, 1};
