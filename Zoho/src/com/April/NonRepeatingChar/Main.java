93dca7c2186828194257ef3f3cc6bf2a236cfd76
keshav <k21keshav05@gmail.com>
2025-09-04 22:52:37 +0530

    Zoho - First non-repeating character in a string

--- Zoho/src/com/April/NonRepeatingChar/Main.java
+++ Zoho/src/com/April/NonRepeatingChar/Main.java
@@ -1,5 +1,16 @@
 package com.April.NonRepeatingChar;
 
+
+/*
+        First Non-Repeating Character in a String
+
+        Conditions
+        1. If no such character exists, return -1. ->  Input: "codeitcodeit" Output: -1
+        2. The comparison is case-sensitive. -> Input: "Codeitcodeit" Output: 'C'
+        3. Special characters and symbols are also counted. -> Input: "codeit@*codeit@" Output: '*'
+
+*/
+
 public class Main {
     public static void main(String[] args) {
 
