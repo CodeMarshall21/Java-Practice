f07e07a4a839495e70562d158423b91ff372b2d9
keshav <k21keshav05@gmail.com>
2025-09-04 15:50:55 +0530

    Zoho - Missing Number - Another problem

---
+++ Zoho/src/com/April/MissingNumber/MissingNumberDynamicRange.java
@@ -0,0 +1,20 @@
+package com.April.MissingNumber;
+/*
+
+Same missing number problem but the range of number is NOT 0 to 1 rather, just an array is given and goal is to find the first missing number
+
+Sample input: 81,84,83,89,85,88
+Sample output: 82 (even though 86, 87 are missing, th goal is to find FIRST MISSING number)
+
+*/
+public class MissingNumberDynamicRange {
+    public static void main(String[] args) {
+
+    }
+
+    private static int missingNumber(int[] arr){
+        int missingNumber = 0;
+        
+        return missingNumber;
+    }
+}
