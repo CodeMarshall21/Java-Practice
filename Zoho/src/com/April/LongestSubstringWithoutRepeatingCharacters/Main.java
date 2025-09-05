81e8c57308cb995c11ebc1dd09c9231e19a94820
keshav <k21keshav05@gmail.com>
2025-09-05 19:20:51 +0530

    Zoho - Longest Substring Without Repeating Characters

---
+++ Zoho/src/com/April/LongestSubstringWithoutRepeatingCharacters/Main.java
@@ -0,0 +1,20 @@
+package com.April.LongestSubstringWithoutRepeatingCharacters;
+
+/*
+            Longest Substring Without Repeating Characters
+
+            Conditions
+            1. Return the length of the longest substring without repeating characters.
+            2. The comparison is case-sensitive.
+            3.All characters, including special characters, are considered.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static int longestNonRepeatingSubstring(String str){
+
+    }
+}
