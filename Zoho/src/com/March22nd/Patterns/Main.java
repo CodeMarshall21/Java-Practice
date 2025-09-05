653148d009738f8e7473b426c726d1c8fcda68b8
keshav <k21keshav05@gmail.com>
2025-09-06 01:08:09 +0530

    Zoho - Patterns

--- Zoho/src/com/March22nd/Patterns/Main.java
+++ Zoho/src/com/March22nd/Patterns/Main.java
@@ -2,6 +2,18 @@ package com.March22nd.Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        
+        pattern(5);
+    }
+
+    private static void pattern(int n){
+
+        for (int row = 1; row <= n ; row++) {
+            int val = row;
+            for (int column = 1; column <= row; column++) {
+                System.out.print(val + " ");
+                val = val + (n - column);
+            }
+            System.out.println();
+        }
     }
 }
