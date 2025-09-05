c02326e7e060bc46791d3cf2e39819b3d0a9207e
keshav <k21keshav05@gmail.com>
2025-09-05 23:56:36 +0530

    Zoho - Leaders In An Array

--- Zoho/src/com/March22nd/LeadersInAnArray/Main.java
+++ Zoho/src/com/March22nd/LeadersInAnArray/Main.java
@@ -1,7 +1,15 @@
 package com.March22nd.LeadersInAnArray;
 
+/*
+            arr = [16,17,4,3,5,2]
+            Output = 17, 5, 2
+            CONSTRAINTS
+            Time Complexity: O(n)
+
+*/
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
