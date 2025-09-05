a4652770fd5689e17dc03a68e52ee7ce97045a0f
keshav <k21keshav05@gmail.com>
2025-09-06 01:30:05 +0530

    Zoho - Largest Number from an Array of Digits

--- Zoho/src/com/March22nd/LargestNumberFromArrayOfDigits/Main.java
+++ Zoho/src/com/March22nd/LargestNumberFromArrayOfDigits/Main.java
@@ -12,6 +12,24 @@ package com.March22nd.LargestNumberFromArrayOfDigits;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {8, 7, 1, 9, 1};
+        System.out.println(largestNumber(arr));
+    }
 
+    private static String largestNumber (int[] arr){
+        StringBuilder largestNumber = new StringBuilder();
+
+        int[] count = new int[10];
+        for (int num : arr) {
+            count[num] += 1;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            if(count[i] >= 1){
+                largestNumber.append(String.valueOf(i).repeat(count[i]));
+            }
+        }
+
+        return largestNumber.toString();
     }
 }
