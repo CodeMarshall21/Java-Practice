73523a030e483e4d9d649e83f03654faa9fa3c77
keshav <k21keshav05@gmail.com>
2025-09-07 00:31:49 +0530

    Find Kth smallest element

--- Zoho/src/com/RepeatedArrayQuestions/FindKthSmallestElement/Main.java
+++ Zoho/src/com/RepeatedArrayQuestions/FindKthSmallestElement/Main.java
@@ -14,8 +14,28 @@ package com.RepeatedArrayQuestions.FindKthSmallestElement;
         Output: 7
 */
 
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    private static int kthSmallestElement(int[] arr, int k) {
+        if (k > arr.length || k == 0) {
+            return -1;
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : arr) {
+            minHeap.add(num);
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
     }
 }
