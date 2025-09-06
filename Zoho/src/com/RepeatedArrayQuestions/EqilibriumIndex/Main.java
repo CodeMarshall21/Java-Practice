59f66470dcd632458db91750d4e993850d139e41
keshav <k21keshav05@gmail.com>
2025-09-07 00:59:56 +0530

    Equilibrium Index

---
+++ Zoho/src/com/RepeatedArrayQuestions/EqilibriumIndex/Main.java
@@ -0,0 +1,16 @@
+package com.RepeatedArrayQuestions.EqilibriumIndex;
+
+/*
+        Equilibrium Index
+        Description:
+        An Equilibrium Index in an array is an index i such that the sum
+        of elements before i is equal to the sum of elements after i.
+
+        arr1 = [3, 4, 9, 6, 1];
+        output = 2
+*/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
