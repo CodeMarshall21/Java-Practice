82dc608d27cb907df73ee265a3cf3952ca1de36a
keshav <k21keshav05@gmail.com>
2025-09-06 22:36:46 +0530

    Find Bitonic Point

--- Zoho/src/com/RepeatedArrayQuestions/FindBitonicPoint/Main.java
+++ Zoho/src/com/RepeatedArrayQuestions/FindBitonicPoint/Main.java
@@ -1,7 +1,19 @@
 package com.RepeatedArrayQuestions.FindBitonicPoint;
 
+/*
+            Find Bitonic Point
+            Description:
+            You are given a bitonic array — an array that first increases
+            and then decreases. Your task is to find the bitonic point — the
+            maximum element in this array.
+
+            Input: [1, 3, 8, 12, 4, 2]
+            Output : 12
+
+*/
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
