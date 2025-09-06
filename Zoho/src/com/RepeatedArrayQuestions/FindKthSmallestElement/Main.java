878f3133e29ae8b6832c650c1e32d7f636ecc294
keshav <k21keshav05@gmail.com>
2025-09-07 00:06:52 +0530

    Find Kth smallest element

---
+++ Zoho/src/com/RepeatedArrayQuestions/FindKthSmallestElement/Main.java
@@ -0,0 +1,21 @@
+package com.RepeatedArrayQuestions.FindKthSmallestElement;
+
+
+/*
+        Find Kth Smallest Element
+        Description:
+        Given an unsorted array and a number k, find the kth smallest
+        element without modifying array.
+
+        Input:
+        arr = [7, 10, 4, 3, 20, 15]
+        k = 3
+
+        Output: 7
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
