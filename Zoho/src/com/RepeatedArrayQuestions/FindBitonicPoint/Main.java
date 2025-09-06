a7a6dcee936404a5c65dd87236837d00c7df7f3c
keshav <k21keshav05@gmail.com>
2025-09-06 23:53:10 +0530

    Find Bitonic Point

--- Zoho/src/com/RepeatedArrayQuestions/FindBitonicPoint/Main.java
+++ Zoho/src/com/RepeatedArrayQuestions/FindBitonicPoint/Main.java
@@ -16,16 +16,19 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[][] testCases = {
-                {1, 3, 8, 12, 4, 2},          // peak in the middle -> 12
-                {1, 2, 3, 4, 5, 4, 3, 2},     // long climb then drop -> 5
-                {10, 20, 30, 40, 50, 40, 30}, // peak at near end -> 50
-                {1, 6, 5},                    // smallest valid bitonic -> 6
-                {5, 10, 20, 15, 5},           // clear peak -> 20
-                {2, 4, 8, 16, 32, 16, 8},     // power sequence -> 32
-                {1, 3, 5, 7, 6, 4, 2},        // peak close to middle -> 7
-                {1, 2, 3, 2, 1},              // symmetric -> 3
+//                {1, 3, 8, 12, 4, 2},          // peak in the middle -> 12
+//                {1, 2, 3, 4, 5, 4, 3, 2},     // long climb then drop -> 5
+//                {10, 20, 30, 40, 50, 40, 30}, // peak at near end -> 50
+//                {1, 6, 5},                    // smallest valid bitonic -> 6
+//                {5, 10, 20, 15, 5},           // clear peak -> 20
+//                {2, 4, 8, 16, 32, 16, 8},     // power sequence -> 32
+//                {1, 3, 5, 7, 6, 4, 2},        // peak close to middle -> 7
+//                {1, 2, 3, 2, 1},              // symmetric -> 3
                 {0, 100, 99, 98, 97},         // sharp rise then fall -> 100
-                {1, 2, 3, 4, 5, 6, 5},        // peak near the end -> 6
+//                {97, 98, 99, 100, 0},         // Gradual rise then sharp fall -> 100
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},  // last peak -> 9
+                {9, 8, 7, 6, 5, 4, 3, 2, 1},  // first peak -> 9
+//                {1, 2, 3, 4, 5, 6, 5},        // peak near the end -> 6
         };
 
         for(int[] arr: testCases){
@@ -58,14 +61,32 @@ public class Main {
                 if(arr[mid - 1] < arr[mid] && arr[mid + 1] < arr[mid]){
                     return arr[mid];
                 }
-                else if(arr[mid - 1] < arr[mid] && arr[mid + 1] > arr[mid]){
+                else if(arr[mid + 1] > arr[mid]){
                     left = mid + 1;
                 }
-                else if(arr[mid - 1] > arr[mid] && arr[mid + 1] < arr[mid]){
+                else{
                     right = mid - 1;
                 }
             }
         }
         return -1;
+    }
+
+
+    private static int bitonicPointOptimized(int[] arr){
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] < arr[mid + 1]){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+
+        }
+        return arr[left];
     }
 }
