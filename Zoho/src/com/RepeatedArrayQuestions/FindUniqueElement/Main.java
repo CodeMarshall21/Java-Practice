b6c0ad7cb16fe7eafcf833d963af1b185d7792e2
keshav <k21keshav05@gmail.com>
2025-09-08 19:22:27 +0530

    Find Unique Element

---
+++ Zoho/src/com/RepeatedArrayQuestions/FindUniqueElement/Main.java
@@ -0,0 +1,20 @@
+package com.RepeatedArrayQuestions.FindUniqueElement;
+
+/*
+        Find Unique Element
+        Description:
+        Given an array where every element appears twice, except
+        one element which appears only once.
+
+        Input = [4, 2, 5, 2, 4]
+        Output = 5
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    private static int uniqueElement(int[] arr){
+        
+    }
+}
