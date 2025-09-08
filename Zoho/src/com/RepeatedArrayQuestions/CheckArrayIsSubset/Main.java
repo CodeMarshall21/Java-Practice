5aac66bc0de8e7621f533b1fbf003a1417b1e723
keshav <k21keshav05@gmail.com>
2025-09-08 17:22:12 +0530

    Check if One Array is a Subset of Another

--- Zoho/src/com/RepeatedArrayQuestions/CheckArrayIsSubset/Main.java
+++ Zoho/src/com/RepeatedArrayQuestions/CheckArrayIsSubset/Main.java
@@ -1,4 +1,26 @@
 package com.RepeatedArrayQuestions.CheckArrayIsSubset;
 
+
+/*
+                Check if One Array is a Subset of Another
+                Description:
+                You're given two arrays: arr1[] and arr2[].
+                You need to check if all elements of arr2 are present in arr1
+                Note: Arrays in sorted order
+
+                Example 1 :-
+                arr1 -[1, 2, 3, 4]
+                arr2 = [2, 4, 5]
+                Output = false
+
+                Example 2:-
+                arr1 -[1, 2, 3, 4, 5]
+                arr2 = [2, 4, 5]
+                Output = true
+*/
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
