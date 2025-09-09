550903fb2d8de8257e1014832a1e97cd6694543d
keshav <k21keshav05@gmail.com>
2025-09-09 15:26:09 +0530

    Check Rotation of a String

--- Zoho/src/com/Others/StringRotation/Main.java
+++ Zoho/src/com/Others/StringRotation/Main.java
@@ -1,7 +1,26 @@
 package com.Others.StringRotation;
 
+
+/*
+                        Check Rotation of a String
+                        Description:
+                        Given two strings s1 and s2 , check if s2 is a rotation of s1
+                        Note: it only allows circular shifting of characters (clockwise)
+
+                        s1 = "abcd"
+                        s2 = "bcda"
+                        Output - True
+
+*/
+
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+    private static boolean isRotated(String str1, String str2){
+        boolean isRotated = false;
+
+
+        return isRotated;
     }
 }
