400848251c59a2f6cdaa5628db9f2f3881a73cf6
keshav <k21keshav05@gmail.com>
2025-11-03 17:32:55 +0530

    getMaximumServices

---
+++ IBM/src/com/practise/question_3/Main.java
@@ -0,0 +1,13 @@
+package com.practise.question_3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getMaximumServices(List<Integer> currentRate){
+
+    }
+}
